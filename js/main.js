bc75fb0bbaf0482310be08a4eb038dac842bdce6
EasyWebDef <64214493+MaxGlinko78@users.noreply.github.com>
2023-04-26 12:10:23 +0300

    New fails

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$('.prosto-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
+
+$('.autoplay').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: false,
+  arrows:false,
+});
+
+
+/*===== modals ======*/
+
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+      modal = document.querySelector('.modal'),
+      modalCloseBtn = document.querySelector('[data-close]');
+
+
+function openModal() {
+   modal.classList.add('show');
+   modal.classList.remove('hide');
+  document.body.style.overflow = 'hidden';
+  clearInterval(modalTimerId);
+}
+
+modalTrigger.forEach(btn =>{
+  btn.addEventListener('click', openModal);
+});
+
+function CloseModal() {
+  modal.classList.add('hide');
+  modal.classList.remove('show');
+  document.body.style.overflow = '';
+}
+  modalCloseBtn.addEventListener('click', CloseModal);
+  
+  modal.addEventListener('click', (e)=>{ 
+    if(e.target === modal){ CloseModal();}
+  
+  document.addEventListener('keydown', (e)=>{
+    if(e.code === "Escape" && modal.classList.contains('show'))
+      { CloseModal(); }
+  });
+
+});
